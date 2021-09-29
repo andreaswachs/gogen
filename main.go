05b91c799b4ba85b2306ed4a41a01de4bf3da847
Andreas Wachs <andreas@wachs.dk>
2021-09-29 20:53:06 +0200

    Feature/configs (#1)
    * Compound commit
    
    * Remove syscall
    
    * Remove config from main
    
    * Update gitignore for golang
    
    * Update yaml scheme
    
    * Add templates folder creation
    
    * remove gogen binary tracking
    
    * Add config parsing to main function

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+/*
+ *
+ *
+ *
+ *
+ */
+
 func main() {
-	Greet()
+	// verbose flag
+	// verbosePtr := flag.Bool("verbose", false, "Enable/disable verbose output")
+
+	flag.Parse()
+
+	tail := flag.Args() // We assume that the tail is the identifier
+	identifier := strings.Join(tail, " ")
+
+	config := GetConfig()
+
+	chosenConfig := Generators{}
+	foundConfig := false
+
+	for i := 0; i < len(config.Generators); i++ {
+		generator := config.Generators[i]
+		if generator.Identifier == identifier {
+			chosenConfig = generator
+			foundConfig = true
+			break
+		}
+	}
+
+	if !foundConfig {
+		fmt.Println("Could not find a definition for given template identifier")
+		os.Exit(3)
+	}
 }

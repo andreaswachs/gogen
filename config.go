90893784856bf0703c7d36c17fb5093fba948f0d
Andreas Wachs Hjalager <ahja@itu.dk>
2021-09-29 21:48:02 +0200

    Add folder and path determination funcs

--- config.go
+++ config.go
@@ -9,6 +9,13 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+const (
+	gogenConfigFolderName  = "/gogen"
+	configFileName         = "/config.yaml"
+	configFileCompletePath = gogenConfigFolderName + configFileName
+	templatesFolderName    = "/templates"
+)
+
 type Settings struct {
 	Verbose             bool
 	RemoveExistingFiles bool
@@ -38,6 +45,60 @@ func GetConfig() YamlStructure {
 	}
 
 	return settings
+}
+
+func GetGogenConfigFolderPath() (string, error) {
+	dir, err := os.UserConfigDir()
+
+	if err != nil {
+		return "", err
+	}
+
+	return dir + gogenConfigFolderName, err
+}
+
+func GetGogenConfigFilePath() (string, error) {
+	dir, err := os.UserConfigDir()
+
+	if err != nil {
+		return "", err
+	}
+
+	return dir + gogenConfigFolderName + configFileName, err
+}
+
+func GetGogenTemplatesFolderPath() (string, error) {
+	dir, err := os.UserConfigDir()
+
+	if err != nil {
+		return "", err
+	}
+
+	return dir + gogenConfigFolderName + templatesFolderName, err
+}
+
+func EnsureConfigFoldersExists() {
+	gogenDir, err := GetGogenConfigFolderPath()
+
+	if err != nil {
+		fmt.Println("Could not determine path for gogen config folder")
+		os.Exit(1)
+	}
+
+	if !ensureNamedFolderExists("gogen", gogenDir) {
+		downloadTemplateConfigFile()
+	}
+
+	templatesDir, err := GetGogenTemplatesFolderPath()
+
+	if err != nil {
+		fmt.Println("Could not determine path for gogen templates folder")
+		os.Exit(1)
+	}
+
+	if !ensureNamedFolderExists("templates", templatesDir) {
+		fmt.Println("The templates folder was just created. The folder is empty. You should put some templates in there!")
+	}
 }
 
 func readConfig() []byte {
@@ -77,22 +138,8 @@ func readConfig() []byte {
 	if _, err := os.Stat(configFileComplete); os.IsNotExist(err) {
 		// download the default configuration
 		fmt.Println("No config file was found locally, will download default config file.")
-		resp, err := http.Get("https://raw.githubusercontent.com/andreaswachs/gogen/main/config.yaml")
 
-		if err != nil {
-			fmt.Println("Failed to download default config file.")
-			os.Exit(2)
-		}
-
-		body, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Println("Failed to read the downloaded default config file.")
-			os.Exit(3)
-		}
-
-		os.WriteFile(configFileComplete, body, os.ModePerm)
-		return body
+		return downloadTemplateConfigFile()
 	}
 
 	config, err := os.ReadFile(configFileComplete)
@@ -104,4 +151,43 @@ func readConfig() []byte {
 	}
 
 	return config
+}
+
+func downloadTemplateConfigFile() []byte {
+	configFile, err := GetGogenConfigFilePath()
+
+	if err != nil {
+		fmt.Println("Could not determine path to config file")
+		os.Exit(1)
+	}
+
+	resp, err := http.Get("https://raw.githubusercontent.com/andreaswachs/gogen/main/config.yaml")
+
+	if err != nil {
+		fmt.Println("Failed to download default config file.")
+		os.Exit(2)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Println("Failed to read the downloaded default config file.")
+		os.Exit(3)
+	}
+
+	os.WriteFile(configFile, body, os.ModePerm)
+	return body
+}
+
+func ensureNamedFolderExists(name string, path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		err := os.MkdirAll(path, os.ModePerm)
+
+		if err != nil {
+			fmt.Printf("Could not create %s folder. This might be an permissions issue.\n", name)
+			os.Exit(1)
+		}
+		return false
+	}
+	return true
 }
